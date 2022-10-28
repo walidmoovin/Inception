94d87fb21246d1f309dcb9f0573f769e6fb4b09a
Walid Bekkal <wbekkal@c3r2p4.42nice.fr>
2022-10-28 21:19:16 +0200

    added wordpress dockerfile + config files + launch script and mariadb dockerfile

---
+++ srcs/requirements/wordpress/config/wp-conf.php
@@ -0,0 +1,8 @@
+/* Database Settings */
+define( 'DB_NAME', 'wordpress-db' );
+define( 'DB_USER', 'wbekkal' )
+define( 'DB_PASSWORD', '1234' );
+define( 'DB_HOST', 'maria-db' );
+
+/* Debug Mode */
+define( 'WP_DEBUG', true );
