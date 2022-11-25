11bfed056c9a842e82359c6482402dcd01b64ef9
Walid Bekkal <wbekkal@c1r1p1.42nice.fr>
2022-11-25 23:39:17 +0100

    df

--- srcs/requirements/mariadb/database-launch.sh
+++ srcs/requirements/mariadb/database-launch.sh
@@ -14,7 +14,7 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
 	mysqld --user=mysql --bootstrap << EOF
 USE mysql;
 FLUSH PRIVILEGES;
-DELETE FROM	mysql.user WHERE User='';
+DELETE FROM	mysql.user WHERE User='';`
 DROP DATABASE test;
 DELETE FROM mysql.db WHERE Db='test';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
